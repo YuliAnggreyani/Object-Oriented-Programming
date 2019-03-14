88a43e3c08915553a0f278a42b7af9bb9e7a2f70
YuliAnggreyani <48541078+YuliAnggreyani@users.noreply.github.com>
2019-03-14 10:37:37 +0700

    Add files via upload

---
+++ yuli_anggreyani_17090113_4C.java
@@ -0,0 +1,10 @@
+
+package output;
+
+public class yuli_anggreyani_17090113_4C{
+    public static void main(String[] args) {
+        System.out.println("selamat datang di object oriented programing \n yuli_anggreyani_17090113_4C");
+    }
+    }
+    
+
